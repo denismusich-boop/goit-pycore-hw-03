77826a424345be54167c2149511385f86298c2ad
Denis <denis.musich@gmail.com>
2026-02-14 19:46:52 +0200

    Update homework-3.py

--- homework-3.py
+++ homework-3.py
@@ -1,2 +1,8 @@
-print("Hello World!")
-print("Hello GIT!")
+from datetime import datetime
+def get_days_from_today(date):
+    given_date = datetime.strptime(date,"%Y-%m-%d").date()
+    today = datetime.now().date()
+    diff = (today - given_date).days
+    return diff
+
+print("days of war:", get_days_from_today("2022-02-24"))
