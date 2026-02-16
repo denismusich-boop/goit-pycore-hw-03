cd0e6f01e2508dd14fac7b233f2a4463d78fd35e
Denis Musich <denis.musich@gmail.com>
2026-02-16 15:06:28 +0200

    task 2 - Implement lottery number generation function
    Added a function to generate a lottery ticket with random numbers.

--- homework-3.py
+++ homework-3.py
@@ -6,3 +6,14 @@ def get_days_from_today(date):
     return diff
 
 print("days of war:", get_days_from_today("2022-02-24"))
+
+
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    if min < 1 or max > 1000 or quantity > (max - min + 1):
+        return []
+    return sorted(random.sample(range(min, max + 1), quantity))
+
+lottery_numbers = get_numbers_ticket(1, 999, 10)
+print("Ваші лотерейні числа:", lottery_numbers)
