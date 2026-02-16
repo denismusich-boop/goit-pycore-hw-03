b79829c70d58b05c57ca013ba5319074e250a83c
Denis Musich <denis.musich@gmail.com>
2026-02-16 17:30:21 +0200

    Implement upcoming birthdays feature
    Added functionality to get upcoming birthdays within a week and adjusted congratulation dates for weekends.

--- homework-3.py
+++ homework-3.py
@@ -47,5 +47,46 @@ print(normalize_phone("38050-123-4567"))
 sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
 print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
 
+Print("Завдання 4")
+
+from datetime import datetime, date, timedelta
+
+def get_upcoming_birthdays(users):
+    today = datetime.today().date()
+    end_date = today + timedelta(days=7)
+    result = []
+
+    for user in users:
+        bday = datetime.strptime(user["birthday"], "%Y.%m.%d").date()
+        birthday_this_year = bday.replace(year=today.year)
+
+        # якщо вже минув — беремо наступний рік
+        if birthday_this_year < today:
+            birthday_this_year = birthday_this_year.replace(year=today.year + 1)
+
+        # перевіряємо, чи входить у вікно [сьогодні; сьогодні+7]
+        if today <= birthday_this_year <= end_date:
+            congratulation_date = birthday_this_year
+
+            # перенос з вихідних на понеділок
+            weekday = congratulation_date.weekday()  # Mon=0 ... Sun=6
+            if weekday == 5:       # Saturday
+                congratulation_date += timedelta(days=2)
+            elif weekday == 6:     # Sunday
+                congratulation_date += timedelta(days=1)
+
+            result.append({
+                "name": user["name"],
+                "congratulation_date": congratulation_date.strftime("%Y.%m.%d")
+            })
+
+    return result
+users = [
+    {"name": "John Doe", "birthday": "1985.01.23"},
+    {"name": "Jane Smith", "birthday": "1990.01.27"},
+    {"name": "Jane Wova", "birthday": "1990.02.21" },
+    {"name": "Ben Down", "birthday": "1999.02.18"},
+]
+print (get_upcoming_birthdays (users))
 
 
