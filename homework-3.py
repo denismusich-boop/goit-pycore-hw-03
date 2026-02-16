5087f729dc1d5d6fb5344ed07110dc38013ba8fb
Denis Musich <denis.musich@gmail.com>
2026-02-16 16:36:13 +0200

    Refactor and add phone number normalization functionality
    Refactor phone number normalization and lottery number generation.

--- homework-3.py
+++ homework-3.py
@@ -17,3 +17,35 @@ def get_numbers_ticket(min, max, quantity):
 
 lottery_numbers = get_numbers_ticket(1, 999, 10)
 print("Ваші лотерейні числа:", lottery_numbers)
+
+
+print("Завдання 3")
+
+import re
+
+def normalize_phone(phone_number):
+    norm = re.sub(r"[^\d+]", "", phone_number)
+
+    if norm.startswith("+"):
+        return norm
+    if norm.startswith("380"):
+        return "+" + norm
+    return "+38" + norm
+
+raw_numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+print(normalize_phone("38050-123-4567"))
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
+
+
+
